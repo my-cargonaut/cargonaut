80489e3006ff71c31c1d01cf8d625a4b0310726f
Lukas Malkmus <lukas.malkmus@mni.thm.de>
2020-07-03 11:34:57 +0200

    Colorful status bar

--- web/src/components/Sidebar.vue
+++ web/src/components/Sidebar.vue
@@ -8,7 +8,7 @@
       expand-on-hover
     >
       <v-list>
-        <v-list-item class="px-2" :to="'/users/' + this.authId">
+        <v-list-item class="px-2" :to="'/users/' + this.authId" color="primary">
           <v-list-item-avatar>
             <v-img :src="'/api/v1/users/' + authId + '/avatar'"></v-img>
           </v-list-item-avatar>
@@ -23,13 +23,13 @@
       <v-divider></v-divider>
 
       <v-list nav dense>
-        <v-list-item to="/trips">
+        <v-list-item to="/trips" color="primary">
           <v-list-item-icon>
             <v-icon>mdi-road-variant</v-icon>
           </v-list-item-icon>
           <v-list-item-title>Trips</v-list-item-title>
         </v-list-item>
-        <v-list-item to="/vehicles">
+        <v-list-item to="/vehicles" color="primary">
           <v-list-item-icon>
             <v-icon>mdi-car</v-icon>
           </v-list-item-icon>
@@ -40,7 +40,7 @@
       <v-divider></v-divider>
 
       <v-list nav dense>
-        <v-list-item @click="logout">
+        <v-list-item @click="logout" color="primary">
           <v-list-item-icon>
             <v-icon>mdi-logout</v-icon>
           </v-list-item-icon>
